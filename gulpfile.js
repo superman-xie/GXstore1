6641681d462ae12c040f139cf1b36d2984c90864
superman-xie <1289277260@qq.com>
2019-03-20 12:10:18 +0800

    gxstore项目初步上传

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const fileinclude = require('gulp-file-include');
+var rev = require('gulp-rev');
+var revCollector = require('gulp-rev-collector');
+gulp.task('fileinclude',function(){
+    gulp.src('src/html/**/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./src/newhtml'))
+})
+gulp.task('sass',()=>{
+	gulp.src('./src/sass/*.scss')
+	.pipe(sass())
+	//.pipe(rename({"suffix" : ".min"}))
+	.pipe(gulp.dest('./src/css'))
+})
+gulp.task('concat', function() {                                //- 创建一个名为 concat 的 task
+    gulp.src('./src/css/*.css')    								//- 需要处理的css文件，放到一个字符串数组里
+        .pipe(concat('index.min.css'))                               //- 合并后的文件名
+        .pipe(minifyCss())                                      //- 压缩处理成一行
+        .pipe(rev())                                            //- 文件名加MD5后缀
+        .pipe(gulp.dest('./dist/css'))                               //- 输出文件本地
+        .pipe(rev.manifest())                                   //- 生成一个rev-manifest.json
+        .pipe(gulp.dest('./rev'));                              //- 将 rev-manifest.json 保存到 rev 目录内
+});
+gulp.task('rev',function(){
+	gulp.src(['./rev/*.json','./src/newhtml/index.html'])
+	.pipe(revCollector({replaceReved: true}))
+	.pipe(gulp.dest('./dist/html/'))
+})
+gulp.task('default',function(){
+	gulp.watch(['./src/sass/*.scss'],['sass'])
+    gulp.watch(['src/html/**/*.html'],['fileinclude']);
+    gulp.watch(['./src/css/*.css'],['concat']);
+    gulp.watch(['./rev/*.json','./src/newhtml/index.html'],['rev']);
+})
